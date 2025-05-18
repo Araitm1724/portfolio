bbab313e4945eff7f6f07ff513ac1fe629776853
荒井智之進 <z3536294@gmail.com>
2025-05-19 08:51:25 +0900

    started to create the portfolio.

---
+++ src/main/java/jp/ne/stars/ss418755/PortfolioApplication.java
@@ -0,0 +1,13 @@
+package jp.ne.stars.ss418755;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/ne/stars/ss418755/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ne.stars.ss418755;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
